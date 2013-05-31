724e474bc85206662df3a0d959297ef7f4e42cd6
wangyang <250801860@qq.com>
2013-05-31 17:58:17 +0800

    Add new file: findNodeClient.erl

---
+++ findnode/src/findNodeClient.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @author flybird <flybird@flybird>
+%%% @copyright (C) 2013, flybird
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30 May 2013 by flybird <flybird@flybird>
+%%%-------------------------------------------------------------------
+-module(findNodeClient).
+
+-author('flybird@flybird').
+
+-compile(export_all).
+
+broadcast() ->
+    mnesia:start(),
+    Address = {226,0,0,1},
+    Port    = 6969,
+    Ttl     = 10,
+
+    Opts1 = [ { active, true },
+	      { ip, Address },
+	      { add_membership, { Address, { 0, 0, 0, 0 } } },
+	      { multicast_loop, true },
+	      { reuseaddr, true },
+	      list ],
+
+    {ok, _RecvSocket} = gen_udp:open(Port, Opts1),
+
+    Opts2 = [ { ip,             { 0, 0, 0, 0 } },
+	      { multicast_ttl,  Ttl }, 
+	      { multicast_loop, true } ],
+    {ok, SendSocket} = gen_udp:open(0, Opts2),
+    
+    ok = gen_udp:send(SendSocket, Address, Port, atom_to_list(node())).
+
