de1a36d6457c40bc2b95fcacef673c7180a16edd
wangyang <250801860@qq.com>
2013-05-31 17:56:14 +0800

    Add new file: common.hrl

---
+++ findnode/src/common.hrl
@@ -0,0 +1,10 @@
+%%%-------------------------------------------------------------------
+%%% @author flybird <flybird@flybird>
+%%% @copyright (C) 2013, flybird
+%%% @doc
+%%%
+%%% @end
+%%% Created : 30 May 2013 by flybird <flybird@flybird>
+%%%-------------------------------------------------------------------
+
+-record(person, {name, age, country}).
